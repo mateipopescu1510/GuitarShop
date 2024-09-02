42255e77923cee5007db2f865e13708598b7dd97
Matei Popescu <popescumateicalin1510@gmail.com>
2024-09-02 23:41:33 +0300

    added unit of work

---
+++ Repositories/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using GuitarShop.Repositories.EmployeeRepository;
+using GuitarShop.Repositories.InstrumentRepository;
+using GuitarShop.Repositories.JobRepository;
+using GuitarShop.Repositories.ShopRepository;
+using GuitarShop.Repositories.UserRepository;
+
+namespace GuitarShop.Repositories.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IEmployeeRepository Employees { get; }
+        IInstrumentRepository Instruments { get; }
+        IJobRepository Jobs { get; }
+        IUserRepository Users { get; }
+        IShopRepository Shops { get; }
+        int Save();
+
+    }
+}
---
+++ Repositories/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using GuitarShop.Data;
+using GuitarShop.Repositories.EmployeeRepository;
+using GuitarShop.Repositories.InstrumentRepository;
+using GuitarShop.Repositories.JobRepository;
+using GuitarShop.Repositories.ShopRepository;
+using GuitarShop.Repositories.UserRepository;
+
+
+namespace GuitarShop.Repositories.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private GuitarShopContext context;
+        public UnitOfWork(GuitarShopContext context) { 
+            this.context = context;
+            Employees = new EmployeeRepository.EmployeeRepository(context);
+            Instruments = new InstrumentRepository.InstrumentRepository(context);
+            Jobs = new JobRepository.JobRepository(context);
+            Users = new UserRepository.UserRepository(context);
+            Shops = new ShopRepository.ShopRepository(context);
+        }
+
+        public IEmployeeRepository Employees {  get; private set; }
+        public IInstrumentRepository Instruments { get; private set; }
+        public IJobRepository Jobs { get; private set; }
+        public IUserRepository Users { get; private set; }
+        public IShopRepository Shops { get; private set; }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+        public int Save()
+        {
+            return context.SaveChanges();
+        }
+
+    }
+}
